11976f7d00068c71d0c0890aab86024720349dc1
zmigrOO <100092805+zmigrOO@users.noreply.github.com>
2024-03-13 17:43:51 +0100

    monte carlo and cosmetics

--- lab1/random.js
+++ lab1/random.js
@@ -1,20 +1,82 @@
 function random_lehmer(x, m, a, seed) {
     x.innerHTML = "";
+    let table = document.createElement("table");
     let arr = [];
     let i = 0;
     while (true) {
         seed = (a * seed) % m;
         if (arr.includes(seed)) {
-            x.innerHTML += "Average = " + (arr.reduce((a, b) => a + b, 0) / i) / m + "<br>";
-            x.innerHTML += "Deviation = " + (1.0-((arr.reduce((a, b) => a + b, 0) / i) / m)/.5) + "<br>";
-            x.innerHTML += "Variation = " + variation(arr, m) + "<br>";
-            x.innerHTML += "Deviation = " + (1.0-(variation(arr, m)/(1/12))) + "<br>";
-            x.innerHTML += "Count = " + i + "<br>";
+            x.appendChild(table);
+            table = document.createElement("table");
+            let row = document.createElement("tr");
+            let cell = document.createElement("td");
+            cell.innerHTML = "Count = " + i;
+            cell.style.border = "1px solid black";
+            row.appendChild(cell);
+            cell = document.createElement("td");
+            cell.innerHTML = "Expected";
+            cell.style.border = "1px solid black";
+            row.appendChild(cell);
+            cell = document.createElement("td");
+            cell.innerHTML = "Actual";
+            cell.style.border = "1px solid black";
+            row.appendChild(cell);
+            cell = document.createElement("td");
+            cell.innerHTML = "Deviation";
+            cell.style.border = "1px solid black";
+            row.appendChild(cell);
+            table.appendChild(row);
+            row = document.createElement("tr");
+            cell = document.createElement("td");
+            cell.innerHTML = "Average";
+            cell.style.border = "1px solid black";
+            row.appendChild(cell);
+            cell = document.createElement("td");
+            cell.innerHTML = "0.5";
+            cell.style.border = "1px solid black";
+            row.appendChild(cell);
+            cell = document.createElement("td");
+            cell.innerHTML = (arr.reduce((a, b) => a + b, 0) / i) / m;
+            cell.style.border = "1px solid black";
+            row.appendChild(cell);
+            cell = document.createElement("td");
+            cell.innerHTML = 1.0-((arr.reduce((a, b) => a + b, 0) / i) / m)/.5;
+            cell.style.border = "1px solid black";
+            row.appendChild(cell);
+            table.appendChild(row);
+            row = document.createElement("tr");
+            cell = document.createElement("td");
+            cell.innerHTML = "Variation";
+            cell.style.border = "1px solid black";
+            row.appendChild(cell);
+            cell = document.createElement("td");
+            cell.innerHTML = "1/12";
+            cell.style.border = "1px solid black";
+            row.appendChild(cell);
+            cell = document.createElement("td");
+            cell.innerHTML = variation(arr, m);
+            cell.style.border = "1px solid black";
+            row.appendChild(cell);
+            cell = document.createElement("td");
+            cell.innerHTML = 1.0-(variation(arr, m)/(1/12));
+            cell.style.border = "1px solid black";
+            row.appendChild(cell);
+            table.appendChild(row);
+            x.appendChild(table);
             break;
         } else {
             arr.push(seed);
         }
-        x.innerHTML += seed +", " +seed/m+ "<br>";
+        let row = document.createElement("tr");
+        let cell1 = document.createElement("td");
+        cell1.style.border = "1px solid black";
+        let cell2 = document.createElement("td");
+        cell2.style.border = "1px solid black";
+        cell1.innerHTML = seed;
+        cell2.innerHTML = seed/m;
+        row.appendChild(cell1);
+        row.appendChild(cell2);
+        table.appendChild(row);
         i++;
     } 
 }
@@ -22,10 +84,11 @@ function random_lehmer_gr(x, m, a, seed) {
     x.innerHTML = "";
     let plot = document.createElement("div");
     plot.style.width = "100%";
-    plot.style.position = "absolute";
     plot.style.top = "0";
+    plot.style.position = "relative";
+    plot.style.overflow = "hidden";
     let divider = document.createElement("div");
-    divider.style.width = "1px";62345
+    divider.style.width = "1px";
     divider.style.height = "100%";
     divider.style.position = "absolute";
     divider.style.left = "50%";
@@ -38,7 +101,6 @@ function random_lehmer_gr(x, m, a, seed) {
         seed = (a * seed) % m;
         if (arr.includes(seed)) {
             // sum of all numbers in the array / i
-            x.innerHTML += "Average = " + (arr.reduce((a, b) => a + b, 0) / i) / m + "<br>";
             let avg = document.createElement("div");
             avg.style.width = "1px";
             avg.style.height = "100%";
@@ -47,10 +109,63 @@ function random_lehmer_gr(x, m, a, seed) {
             avg.style.left = 100*(arr.reduce((a, b) => a + b, 0) / i) / m + "%";
             avg.style.borderLeft = "1px solid red";
             plot.appendChild(avg);
-            x.innerHTML += "Deviation = " + (1.0-((arr.reduce((a, b) => a + b, 0) / i) / m)/.5) + "<br>";
-            x.innerHTML += "Variation = " + variation(arr, m) + "<br>";
-            x.innerHTML += "Deviation = " + (1.0-(variation(arr, m)/(1/12))) + "<br>";
-            x.innerHTML += "Count = " + i + "<br>";
+            x.appendChild(plot);
+            let table = document.createElement("table");
+            let row = document.createElement("tr");
+            let cell = document.createElement("td");
+            cell.innerHTML = "Count = " + i;
+            cell.style.border = "1px solid black";
+            row.appendChild(cell);
+            cell = document.createElement("td");
+            cell.innerHTML = "Expected";
+            cell.style.border = "1px solid black";
+            row.appendChild(cell);
+            cell = document.createElement("td");
+            cell.innerHTML = "Actual";
+            cell.style.border = "1px solid black";
+            row.appendChild(cell);
+            cell = document.createElement("td");
+            cell.innerHTML = "Deviation";
+            cell.style.border = "1px solid black";
+            row.appendChild(cell);
+            table.appendChild(row);
+            row = document.createElement("tr");
+            cell = document.createElement("td");
+            cell.innerHTML = "Average";
+            cell.style.border = "1px solid black";
+            row.appendChild(cell);
+            cell = document.createElement("td");
+            cell.innerHTML = "0.5";
+            cell.style.border = "1px solid black";
+            row.appendChild(cell);
+            cell = document.createElement("td");
+            cell.innerHTML = (arr.reduce((a, b) => a + b, 0) / i) / m;
+            cell.style.border = "1px solid black";
+            row.appendChild(cell);
+            cell = document.createElement("td");
+            cell.innerHTML = 1.0-((arr.reduce((a, b) => a + b, 0) / i) / m)/.5;
+            cell.style.border = "1px solid black";
+            row.appendChild(cell);
+            table.appendChild(row);
+            row = document.createElement("tr");
+            cell = document.createElement("td");
+            cell.innerHTML = "Variation";
+            cell.style.border = "1px solid black";
+            row.appendChild(cell);
+            cell = document.createElement("td");
+            cell.innerHTML = "1/12";
+            cell.style.border = "1px solid black";
+            row.appendChild(cell);
+            cell = document.createElement("td");
+            cell.innerHTML = variation(arr, m);
+            cell.style.border = "1px solid black";
+            row.appendChild(cell);
+            cell = document.createElement("td");
+            cell.innerHTML = 1.0-(variation(arr, m)/(1/12));
+            cell.style.border = "1px solid black";
+            row.appendChild(cell);
+            table.appendChild(row);
+            x.appendChild(table);
             break;
         } else {
             arr.push(seed);
@@ -64,7 +179,6 @@ function random_lehmer_gr(x, m, a, seed) {
         plot.appendChild(div);
         i++;
     } 
-    x.appendChild(plot);
 }
 function variation(array, m) {
     let sum = 0;
@@ -75,30 +189,85 @@ function variation(array, m) {
     return sum / array.length;
 }
 function random_vonneumann(x, seed, iterations) {
-    x.innerHTML = "";
     function middleSquare() {
         let s = (seed**2).toString();
         while (s.length != 8) {
             s = "0" + s;
         }
         seed = parseInt(s.substring(2, 6));
-        console.log(s);
-        x.innerHTML += s +", "+ seed + "<br>";
-        return;
+        let row = document.createElement("tr");
+        let cell = document.createElement("td");
+        cell.innerHTML = s;
+        cell.style.border = "1px solid black";
+        row.appendChild(cell);        
+        table.appendChild(row);
+        cell = document.createElement("td");
+        cell.innerHTML = seed/10000;
+        cell.style.border = "1px solid black";
+        row.appendChild(cell);
+        return row;
     }
+    x.innerHTML = "";
+    let table = document.createElement("table");
     for (let i = 0; i < iterations; i++) {
-        middleSquare();
+        table.appendChild(middleSquare());
     }
+    x.appendChild(table);
 }
 function pi_montecarlo(n, p) {
+    p.innerHTML = "";
+    let plot = document.createElement("div");
+    plot.style.height = "100%";
+    plot.style.aspectRatio = "1/1";
+    plot.style.position = "absolute";
+    plot.style.top = "0";
+    plot.style.left = "right";
+    let dividerx = document.createElement("div");
+    dividerx.style.width = "1px";
+    dividerx.style.height = "100%";
+    dividerx.style.position = "absolute";
+    dividerx.style.left = "50%";
+    dividerx.style.borderLeft = "1px solid black";
+    dividerx.innerHTML = "0";
+    plot.appendChild(dividerx);
+    let dividery = document.createElement("div");
+    dividery.style.width = "100%";
+    dividery.style.height = "1px";
+    dividery.style.position = "absolute";
+    dividery.style.top = "50%";
+    dividery.style.borderTop = "1px solid black";
+    dividery.innerHTML = "0";
+    plot.appendChild(dividery);
     let inside = 0;
     for (let i = 0; i < n; i++) {
-        let x = Math.random();
-        let y = Math.random();
+        let x = Math.random()*2-1;
+        let y = Math.random()*2-1;
+        let div = document.createElement("div");
+        div.style.position = "absolute";
+        div.style.width = "10px";
+        div.style.height = "10px";
+        div.style.left = 50*x+50 + "%";
+        div.style.top = 50*y+50 + "%";
+        div.style.backgroundColor = "gray";
+        div.style.opacity = "1";
+        div.style.borderRadius = "50%";
         if (x * x + y * y < 1) {
             inside++;
+            div.style.backgroundColor = "blue"; 
+            div.style.opacity = "1";    
         }
+        plot.appendChild(div);
     }
-    p.innerHTML = (4 * inside / n).toString();
-    
+    let pi = document.createElement("div");
+    pi.innerHTML = "π = " + (4 * inside / n).toString();
+    pi.style.backgroundColor = "white";
+    pi.style.height = "fit-content";
+    pi.style.width = "fit-content";
+    pi.style.position = "absolute";
+    pi.style.bottom = "0";
+    pi.style.left = "0";
+    pi.style.border = "1px solid black";
+
+    plot.appendChild(pi);
+    p.appendChild(plot);
 }
